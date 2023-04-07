af8dc360bb8ecf92a944d24287cebc53db098949
Akanksha <nehetea@mcmaster.ca>
2023-04-07 10:59:47 -0400

    added menu and switching between menu functionality

---
+++ carpoolapp/app/src/main/java/com/example/carpoolapp/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.carpoolapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.carpoolapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.BottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+                R.id.homeMenu -> replaceFragment(HomeFragment())
+                R.id.requestRideMenu -> replaceFragment(RequestRideFragment())
+                R.id.offerRideMenu -> replaceFragment(OfferRideFragment())
+                R.id.settingsMenu -> replaceFragment(ProfileFragment())
+                else ->{
+                }
+
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+}
