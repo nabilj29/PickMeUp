78708b0c6a6f4755c06a41ef14cefbbbae2e1b20
Mohaansh Pranjal <58990171+mohaanshpranjal@users.noreply.github.com>
2023-04-07 12:43:22 -0400

    added user profile

---
+++ carpoolapp/app/src/main/java/com/example/carpoolapp/Model/User.kt
@@ -0,0 +1,10 @@
+package com.example.carpoolapp.Model
+
+data class User (
+    var username: String,
+    var fullname: String,
+    var phone: Int,
+    var rating: Double,
+    var email: String,
+    var address: String
+        )
