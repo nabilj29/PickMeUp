66e97f8ceeb2b64a294567e1ba2a4fcba86ddc20
Akanksha <nehetea@mcmaster.ca>
2023-04-09 19:11:57 -0400

    added starting page

---
+++ carpoolapp/app/src/main/java/com/example/carpoolapp/StartingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.carpoolapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_starting)
+    }
+}
