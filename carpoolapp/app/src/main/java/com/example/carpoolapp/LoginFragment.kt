d26f96d767e5af76f0c77a93d9a33cdf71235f95
Akanksha <nehetea@mcmaster.ca>
2023-04-09 21:17:55 -0400

    pushed login page transition

--- carpoolapp/app/src/main/java/com/example/carpoolapp/LoginFragment.kt
+++ carpoolapp/app/src/main/java/com/example/carpoolapp/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentTransaction
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +35,8 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val v = inflater.inflate(R.layout.fragment_login, container, false)
+        return v
     }
 
     companion object {
--- carpoolapp/app/src/main/java/com/example/carpoolapp/StartingActivity.kt
+++ carpoolapp/app/src/main/java/com/example/carpoolapp/StartingActivity.kt
@@ -2,10 +2,25 @@ package com.example.carpoolapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.carpoolapp.databinding.ActivityMainBinding
+import androidx.fragment.app.FragmentTransaction
 
 class StartingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_starting)
+        replaceFragment(StartingFragment())
+
+
+    }
+
+
+    private fun replaceFragment(fragment : Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.FragmentContainer, fragment)
+        fragmentTransaction.commit()
     }
 }
+
